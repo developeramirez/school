5079c626115c5ed37429ad09d56e83fddbd33f24
Allan <allanramirez.edutec@gmail.com>
2019-06-18 15:43:32 -0600

    modificacion de archivo create

--- template/Students/create.php
+++ template/Students/create.php
@@ -4,16 +4,22 @@
 	<title></title>
 </head>
 <body>
-<form>
-	<input type="text" name="txtName"> <br><br>
-	<input type="date" name="txtBirth"><br><br>
-	<select>
-		<option value="1">Activo</option>
-		<option value="0">Inactivo</option>
-	</select>>
-	
+<div class="field">
+	<form class="form">
+		<input type="text" name="txtName" class="input" placeholder="FullName"> <br><br>
+		<input type="date" name="txtBirth" class="input"><br><br>
+		<div class="select">
+			<div class="control">
+				<select>
+					<option>State</option>
+					<option value="1">Activo</option>
+					<option value="0">Inactivo</option>
+				</select>
+			</div>	
+		</div><br><br>
+		<button class="button is-link">Save Student</button>
 
-
-</form>
+	</form>
+</div>
 </body>
 </html>
